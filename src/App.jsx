4b854f890e67bcb82408580cf92d0027315240f5
Me-shefali <160989393+Me-shefali@users.noreply.github.com>
2026-01-03 13:47:33 +0530

    Built the file structure for the portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
+import {Navbar} from '@/layout/Navbar'
+import {Hero} from '@/sections/Hero'
+import {Projects} from '@/sections/Projects'
+import {Experience} from '@/sections/Experience'
+import {About} from '@/sections/About'
+import {Contact} from '@/sections/Contact'
+import {Testimonials} from '@/sections/Testimonials'
+
 function App() {
 
   return (
-    <>
-
-    </>
+    <div className='min-h-screen overflow-x-hidden'>
+      <Navbar />
+      <main>
+        <Hero />
+        <About />
+        <Experience />
+        <Projects />
+        <Testimonials />
+        <Contact />
+      </main>
+    </div>
   )
 }
 
