aab36b9cb2d5a0ecdbd02c33ac2df44cbe74b3a7
Me-shefali <160989393+Me-shefali@users.noreply.github.com>
2026-01-09 13:08:44 +0530

    Completed the profile section

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1.5rem",
+        sm: "2rem",
+        md: "3rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
+};
